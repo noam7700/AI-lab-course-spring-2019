224a2dc029804052fd615e20f092f385d4e89648
Noam Blagovsky <noam7700@gmail.com>
2019-03-24 18:14:41 +0200

    nothing important -- main.py can now run from cmd w/o import issues

---
+++ ai_lab2/BiDirectionalAStar.py
@@ -0,0 +1,8 @@
+from AStar import FibonacciHeapWithHashTable
+from AStar import Node
+
+
+
+
+def bi_directional_a_star(start_game_state, end_game_state, h_func, g_func, max_time_solution):
+    
--- ai_lab2/main.py
+++ ai_lab2/main.py
@@ -1,11 +1,11 @@
+import sys
+
+sys.path.insert(0, "C:/Users/nir blagovsky/PycharmProjects/ai_lab/ai_lab1")  # lab2 extends lab1
 
 
 def main():
     print("Hello ai_lab2!")
 
 
-
-
 if __name__ == '__main__':
     main()
-

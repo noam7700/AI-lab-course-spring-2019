9165a655496963e004ee44b261de84449f74e76f
Noam Blagovsky <noam7700@gmail.com>
2019-03-27 18:19:51 +0200

    IDA* finished I think (after created output file and such...)

--- ai_lab2/IDA_star.py
+++ ai_lab2/IDA_star.py
@@ -10,6 +10,13 @@ from time import time
 
 def ida_star(start_game_state, f_func, max_time_solution):
 
+
+    # N = num_searched* - sum of num_searched in every iteration
+    # permit - d/N
+    # exec_time ...
+    # ebf - N^(1/d)
+    N = 0
+
     Solution = namedtuple("Solution",
                           ["solution_node", "num_searched", "permit", "exec_time", "ebf", "d"])
 
@@ -22,11 +29,14 @@ def ida_star(start_game_state, f_func, max_time_solution):
 
     while time() - start_time < max_time_solution:  # loop cutoff from 1 to inf
         [new_cutoff, solution] = dls(start_game_state, cutoff, f_func, start_time, max_time_solution)
+
+        N += solution.num_searched
+
         if solution.solution_node is not None:
             break
         cutoff = new_cutoff
     # found solution! (or it got to max_time, but who cares! :P - it still returns some data...)
-    return solution
+    return Solution(solution.solution_node, N, solution.d / N, solution.exec_time, pow(N, 1/solution.d), solution.d)
 
 def f_func(node):
     # heuristic2 assumes node.game_state.board

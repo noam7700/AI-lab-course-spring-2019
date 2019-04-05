3b156fc5b20a75a9aecb095e4fe90872a821a935
Noam Blagovsky <noam7700@gmail.com>
2019-04-05 20:40:03 +0300

    freedom heuristic (Question Aleph 3a+b)

--- ai_lab2/NewHeuristics.py
+++ ai_lab2/NewHeuristics.py
@@ -145,3 +145,164 @@ def heuristic2_degree2(node):
                         seen_blockersx2[node.game_state.board[i][j]] = 1
 
     return blockers + blockers_of_blockers
+
+def freedom(node):
+    c = 4  # as explained in the pdf file. definiton of blocking car
+    d = 1  # "factor of penalty". the bigger the d, the bigger the impact of the heuristic on the search.
+    return freedom_c(node, c, d)
+
+def freedom_c(node, c, d):
+
+    if node.createdMove is None:
+        return 0
+
+    last_action = node.createdMove  # for example, AR4
+    car_name = last_action[0]
+    direction = last_action[1]
+    steps = int(last_action[2])
+
+    # find start position. searching up-to-down & left-to-right.
+    start_row = 0
+    start_column = 0
+    didBreak = 0  # break from outside for
+    for i in range(GameState.GameState.dimY):
+        for j in range(GameState.GameState.dimX):
+            if node.game_state.board[i][j] is car_name:
+                start_row = i
+                start_column = j
+                didBreak = 1
+                break
+        if didBreak is 1:
+            break
+
+    # find end position.
+    end_column = start_column
+    end_row = start_row
+    car_length = 0
+    if direction is 'R' or direction is 'L':  # search right
+        while end_column < GameState.GameState.dimX and node.game_state.board[start_row][end_column] is car_name:
+            end_column += 1
+        end_column -= 1  # we went one too far with end_column
+        car_length = abs(end_column - start_column) + 1
+    else:  # search down
+        while end_row < GameState.GameState.dimY and node.game_state.board[end_row][start_column] is car_name:
+            end_row += 1
+        end_row -= 1  # we went one too far with end_row
+
+        car_length = abs(end_row - start_row) + 1
+
+    # prev_start & prev_end
+    if direction is 'R':
+        prev_start_row = start_row
+        prev_end_row = start_row
+        prev_start_column = start_column - steps
+        prev_end_column = prev_start_column + car_length - 1
+    elif direction is 'L':
+        prev_start_row = start_row
+        prev_end_row = start_row
+        prev_start_column = start_column + steps
+        prev_end_column = prev_start_column + car_length - 1
+    elif direction is 'U':
+        prev_start_row = start_row + steps
+        prev_end_row = prev_start_row + car_length - 1
+        prev_start_column = start_column
+        prev_end_column = start_column
+    elif direction is 'D':
+        prev_start_row = start_row - steps
+        prev_end_row = prev_start_row + car_length - 1
+        prev_start_column = start_column
+        prev_end_column = start_column
+
+
+    # I know there are some code duplications. but it would complicate it if we would try to save some code
+    prev_blocked_cars = 0
+    blocked_cars = 0
+    if direction is 'R' or direction is 'L':  # horizontal
+        # count number of freed cars - pixels of previous position
+        for j in range(prev_start_column, prev_end_column + 1):  # exapmle: AA... --AR3--> ...AA
+            prev_counted_car = '~'  # don't re-count a car twice
+            for i in range(max(prev_start_row-c, 0), prev_start_row):  # freed cars from up
+                if node.game_state.board[i][j] is not '.' and node.game_state.board[i][j] is not prev_counted_car:
+                    isVertical = (i+1 < GameState.GameState.dimY) and node.game_state.board[i][j] is node.game_state.board[i+1][j]
+                    isVertical = isVertical or (i-1 >= 0 and node.game_state.board[i][j] is node.game_state.board[i-1][j])
+                    if isVertical:  # it blocks it's movement only if it's vertical
+                        prev_blocked_cars += 1
+                        prev_counted_car = node.game_state.board[i][j]
+
+            prev_counted_car = '~'  # don't re-count a car twice
+            for i in range(min(prev_start_row+c, GameState.GameState.dimY-1), prev_start_row, -1):  # freed cars from down
+                if node.game_state.board[i][j] is not '.' and node.game_state.board[i][j] is not prev_counted_car:
+                    isVertical = (i + 1 < GameState.GameState.dimY) and node.game_state.board[i][j] is node.game_state.board[i + 1][j]
+                    isVertical = isVertical or (i - 1 >= 0 and node.game_state.board[i][j] is node.game_state.board[i - 1][j])
+                    if isVertical:  # it blocks it's movement only if it's vertical
+                        prev_blocked_cars += 1
+                        prev_counted_car = node.game_state.board[i][j]
+
+        # count number of blocked cars - pixels of current position
+        for j in range(start_column, end_column + 1):  # exapmle: AA... --AR3--> ...AA
+            prev_counted_car = '~'  # don't re-count a car twice
+            for i in range(max(start_row-c, 0), start_row):  # blocked cars from up
+                if node.game_state.board[i][j] is not '.' and node.game_state.board[i][j] is not prev_counted_car:
+                    isVertical = (i + 1 < GameState.GameState.dimY) and node.game_state.board[i][j] is node.game_state.board[i + 1][j]
+                    isVertical = isVertical or (i - 1 >= 0 and node.game_state.board[i][j] is node.game_state.board[i - 1][j])
+                    if isVertical:  # it blocks it's movement only if it's vertical
+                        blocked_cars += 1
+                        prev_counted_car = node.game_state.board[i][j]
+
+            prev_counted_car = '~'  # don't re-count a car twice
+            for i in range(min(start_row+c, GameState.GameState.dimY-1), start_row, -1):  # blocked cars from down
+                if node.game_state.board[i][j] is not '.' and node.game_state.board[i][j] is not prev_counted_car:
+                    isVertical = (i + 1 < GameState.GameState.dimY) and node.game_state.board[i][j] is node.game_state.board[i + 1][j]
+                    isVertical = isVertical or (i - 1 >= 0 and node.game_state.board[i][j] is node.game_state.board[i - 1][j])
+                    if isVertical:  # it blocks it's movement only if it's vertical
+                        blocked_cars += 1
+                        prev_counted_car = node.game_state.board[i][j]
+
+    elif direction is 'U' or direction is 'D':  # condition isn't really necessary, it's else
+        # count number of freed cars - pixels of previous position
+        for i in range(prev_start_row, prev_end_row + 1):
+            prev_counted_car = '~'  # don't re-count a car twice
+            for j in range(max(start_column-c, 0), start_column):  # freed cars from left
+                if node.game_state.board[i][j] is not '.' and node.game_state.board[i][j] is not prev_counted_car:
+                    isHorizontal = (j+1 < GameState.GameState.dimX) and node.game_state.board[i][j] is node.game_state.board[i][j + 1]
+                    isHorizontal = isHorizontal or (j-1 >= 0 and node.game_state.board[i][j] is node.game_state.board[i][j - 1])
+                    if isHorizontal:  # it blocks it's movement only if it's horizontal
+                        prev_blocked_cars += 1
+                        prev_counted_car = node.game_state.board[i][j]
+
+            prev_counted_car = '~'  # don't re-count a car twice
+            for j in range(min(start_column+c, GameState.GameState.dimX-1), start_column, -1):  # freed cars from right
+                if node.game_state.board[i][j] is not '.' and node.game_state.board[i][j] is not prev_counted_car:
+                    isHorizontal = (j+1 < GameState.GameState.dimX) and node.game_state.board[i][j] is node.game_state.board[i][j + 1]
+                    isHorizontal = isHorizontal or (j-1 >= 0 and node.game_state.board[i][j] is node.game_state.board[i][j - 1])
+                    if isHorizontal:  # it blocks it's movement only if it's horizontal
+                        prev_blocked_cars += 1
+                        prev_counted_car = node.game_state.board[i][j]
+
+        # count number of blocked cars - pixels of current position
+        for i in range(start_row, end_row + 1):
+            prev_counted_car = '~'  # don't re-count a car twice
+            for j in range(max(start_column - c, 0), start_column):  # blocked cars from left
+                if node.game_state.board[i][j] is not '.' and node.game_state.board[i][j] is not prev_counted_car:
+                    isHorizontal = (j+1 < GameState.GameState.dimX) and node.game_state.board[i][j] is node.game_state.board[i][j + 1]
+                    isHorizontal = isHorizontal or (j-1 >= 0 and node.game_state.board[i][j] is node.game_state.board[i][j - 1])
+                    if isHorizontal:  # it blocks it's movement only if it's horizontal
+                        blocked_cars += 1
+                        prev_counted_car = node.game_state.board[i][j]
+
+            prev_counted_car = '~'  # don't re-count a car twice
+            for j in range(min(start_column + c, GameState.GameState.dimX), start_column):  # blocked cars from right
+                if node.game_state.board[i][j] is not '.' and node.game_state.board[i][j] is not prev_counted_car:
+                    isHorizontal = (j+1 < GameState.GameState.dimX) and node.game_state.board[i][j] is node.game_state.board[i][j + 1]
+                    isHorizontal = isHorizontal or (j-1 >= 0 and node.game_state.board[i][j] is node.game_state.board[i][j - 1])
+                    if isHorizontal:  # it blocks it's movement only if it's horizontal
+                        blocked_cars += 1
+                        prev_counted_car = node.game_state.board[i][j]
+
+    last_action_heuristic_cost = blocked_cars - prev_blocked_cars
+    prev_heuristic = node.score - node.cost_to_root
+    # d is the factor of penalty
+    return prev_heuristic + d * last_action_heuristic_cost
+
+def freedom_and_heu2deg2(node):
+    return 0.3 * freedom(node) + heuristic2_degree2(node)

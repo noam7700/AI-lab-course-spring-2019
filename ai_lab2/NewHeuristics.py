7362640c69607c5bdab6de5d734a90568e174d0f
Noam Blagovsky <noam7700@gmail.com>
2019-04-05 07:19:38 +0300

    heuristic2 + blockers of degree of 2 (Question alepth 2)
    we don't allow re-counting of some car as a blocker of two blockers

--- ai_lab2/NewHeuristics.py
+++ ai_lab2/NewHeuristics.py
@@ -57,3 +57,91 @@ def sum_distances_of_blocking_cars(node):
 
     return sum_distances
 
+def heuristic2_degree2(node):
+    # result = #blockers + #blockers_of_blockers
+    # if a blocker is a trunk, her blockers are only cars that block downwards (only option...)
+    # if a blocker is a car, her blockers is her best option (be optimistic)
+    blockers = 0
+    blockers_of_blockers = 0
+
+    # if blocker of blocker was counted for blocking previous blocker. don't re-count him.
+    # for example, if some car, blocks 2 blockers. she can with 1 step, un-block them!
+    seen_blockersx2 = {}
+
+    x_row = 2
+    x_end_column = 0
+
+    # searching for X_car's end_pos because we are no longer keeping Car objects in game_state
+    for j in range(GameState.GameState.dimX - 1, -1, -1):  # from dimX - 1 to 0
+        if node.game_state.board[x_row][j] is 'X':  # found X!
+            x_end_column = j
+            break
+
+    for j in range(x_end_column+1, GameState.GameState.dimX):
+        if node.game_state.board[x_row][j] is not '.':  # found a blocker
+            blockers += 1
+            car_name = node.game_state.board[x_row][j]
+
+            row_start = 0
+            row_end = 0
+
+            for i in range(x_row - 1, -1, -1):  # check how much distance to move the car downwards
+                if i is 0:  # special case!
+                    if node.game_state.board[i][j] is car_name:
+                        row_start = i
+                    else:
+                        row_start = i + 1
+
+                if node.game_state.board[i][j] is not car_name:
+                    row_start = i + 1  # previous pixel was his end
+                    break
+
+            for i in range(x_row + 1, GameState.GameState.dimY):
+                if i is GameState.GameState.dimY - 1:  # special case!
+                    if node.game_state.board[i][j] is car_name:
+                        row_start = i
+                    else:
+                        row_start = i - 1
+
+                if node.game_state.board[i][j] is not car_name:
+                    row_end = i - 1
+                    break
+
+            # to recognize if it's a trunk or a car
+            car_length = row_end - row_start + 1
+
+            # blockers of downwards:
+            down = 0
+            up = 0
+            for i in range(row_end+1, GameState.GameState.dimY):
+                # blocker of blocker that wasn't counted yet
+                if node.game_state.board[i][j] is not '.' and not node.game_state.board[i][j] in seen_blockersx2:
+                    down += 1
+            for i in range(row_start-1, -1, -1):
+                # blocker of blocker that wasn't counted yet
+                if node.game_state.board[i][j] is not '.' and not node.game_state.board[i][j] in seen_blockersx2:
+                    up += 1
+
+            isUp = -1  # which cars should I add to hash (seen_blockersx2)
+            if car_length is 3:  # trunk can only clear the way if it goes down.
+                blockers_of_blockers += down
+                isUp = 0
+            else:  # car can go up/down to clear the way. we're optimistic
+                if down < up:
+                    blockers_of_blockers += down
+                    isUp = 0
+                else:
+                    blockers_of_blockers += up
+                    isUp = 1
+
+            if isUp is 1:
+                # add upwards blockers of blockers to hash
+                for i in range(row_start - 1, -1, -1):
+                    if node.game_state.board[i][j] is not '.':
+                        seen_blockersx2[node.game_state.board[i][j]] = 1
+            else:
+                for i in range(row_end + 1, GameState.GameState.dimY):
+                    if node.game_state.board[i][j] is not '.':
+                        seen_blockersx2[node.game_state.board[i][j]] = 1
+
+    return blockers + blockers_of_blockers
